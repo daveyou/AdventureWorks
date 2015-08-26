60855631ff1c04c53a10f571d7e85bcfee78bb7c
David You <david.you@red-gate.com>
2015-08-26 14:39:47 -0700

    linked static data to repo

---
+++ Data/Person.AddressType_Data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [Person].[AddressType] ON
+INSERT INTO [Person].[AddressType] ([AddressTypeID], [Name], [rowguid], [ModifiedDate]) VALUES (6, N'Senior Developer', 'a67f238a-5ba2-444b-966c-0467ed9c427f', '2015-08-26 14:38:11.040')
+SET IDENTITY_INSERT [Person].[AddressType] OFF
---
+++ Data/Person.ContactType_Data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [Person].[ContactType] ON
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1, N'Senior Developer', '2015-08-26 14:31:38.540')
+SET IDENTITY_INSERT [Person].[ContactType] OFF
