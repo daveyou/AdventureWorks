759694cf61ab4534c012405d03cd6c0d208a1fbe
David You <david.you@red-gate.com>
2015-08-28 09:53:12 -0700

    added new data

--- Data/Person.ContactType_Data.sql
+++ Data/Person.ContactType_Data.sql
@@ -1,3 +1,6 @@
 SET IDENTITY_INSERT [Person].[ContactType] ON
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (2, N'Principal Consultant', '2015-08-28 09:48:40.533')
+SET IDENTITY_INSERT [Person].[ContactType] OFF
+SET IDENTITY_INSERT [Person].[ContactType] ON
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1, N'Senior Developer', '2015-08-26 14:31:38.540')
 SET IDENTITY_INSERT [Person].[ContactType] OFF
