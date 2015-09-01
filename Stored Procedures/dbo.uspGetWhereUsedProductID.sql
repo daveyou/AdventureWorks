02989794d2741d2c64e11e4491f58d6caf4a0aa3
David You <david.you@red-gate.com>
2015-09-01 15:24:05 -0700

    modified sproc

--- Stored Procedures/dbo.uspGetWhereUsedProductID.sql
+++ Stored Procedures/dbo.uspGetWhereUsedProductID.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -31,9 +32,10 @@ SELECT b.[ProductAssemblyID], b.[ComponentID], b.[ComponentDesc], SUM(b.[PerAsse
 FROM [BOM_cte] b
 GROUP BY b.[ComponentID], b.[ComponentDesc], b.[ProductAssemblyID], b.[BOMLevel], b.[RecursionLevel], b.[StandardCost], b.[ListPrice]
 ORDER BY b.[BOMLevel], b.[ProductAssemblyID], b.[ComponentID]
-OPTION (MAXRECURSION 50) 
+OPTION (MAXRECURSION 75) 
 END;
 GO
+
 EXEC sp_addextendedproperty N'MS_Description', N'Stored procedure using a recursive query to return all components or assemblies that directly or indirectly use the specified ProductID.', 'SCHEMA', N'dbo', 'PROCEDURE', N'uspGetWhereUsedProductID', NULL, NULL
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Input parameter for the stored procedure uspGetWhereUsedProductID used to eliminate components not used after that date. Enter a valid date.', 'SCHEMA', N'dbo', 'PROCEDURE', N'uspGetWhereUsedProductID', 'PARAMETER', N'@CheckDate'
