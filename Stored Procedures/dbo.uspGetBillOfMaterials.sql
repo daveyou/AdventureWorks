22d52275b7a11bdc199f7bd9569a132d0c8c347f
David You <david.you@red-gate.com>
2015-09-01 13:44:45 -0700

    modified sproc

--- Stored Procedures/dbo.uspGetBillOfMaterials.sql
+++ Stored Procedures/dbo.uspGetBillOfMaterials.sql
@@ -32,9 +32,10 @@ SELECT b.[ProductAssemblyID], b.[ComponentID], b.[ComponentDesc], SUM(b.[PerAsse
 FROM [BOM_cte] b
 GROUP BY b.[ComponentID], b.[ComponentDesc], b.[ProductAssemblyID], b.[BOMLevel], b.[RecursionLevel], b.[StandardCost], b.[ListPrice]
 ORDER BY b.[BOMLevel], b.[ProductAssemblyID], b.[ComponentID]
-OPTION (MAXRECURSION 70) 
+OPTION (MAXRECURSION 50) 
 END;
 GO
+
 
 EXEC sp_addextendedproperty N'MS_Description', N'Stored procedure using a recursive query to return a multi-level bill of material for the specified ProductID.', 'SCHEMA', N'dbo', 'PROCEDURE', N'uspGetBillOfMaterials', NULL, NULL
 GO
